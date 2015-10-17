cd1b895a048391a829d655eacbe4801cc08d2f18
volaka <hasancanvolaka@gmail.com>
2015-10-17 21:51:33 +0300

    argumanlar ayarlandi. 3 arguman girilmezse program hata ile sonlaniyor.

--- odev03/odev03_thread.py
+++ odev03/odev03_thread.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+
 alphabet="abcdefghijklmnopqrstuvwxyz"
 alphabet_len=len(alphabet)
 
+
+if len(sys.argv) != 4:
+	print "Usage: python odev03_thread.py <shifting> <threads> <block_length>"
+	sys.exit("Invalid arguments")
+else:
+	s=sys.argv[1]
+	n=sys.argv[2]
+	l=sys.argv[3]
 
