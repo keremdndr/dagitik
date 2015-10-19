97b74cbf10782fedddfdc2e50c162c48597a64b9
volaka <hasancanvolaka@gmail.com>
2015-10-19 19:04:15 +0300

    kuyruga eklerken kilit alma kaldirildi.

--- odev03/odev03_thread.py
+++ odev03/odev03_thread.py
@@ -3,6 +3,7 @@
 import sys
 import threading
 import Queue
+import time
 
 class reader_thread(threading.Thread):
 	def __init__(self,threadID,name,file_object,read_queue,write_queue,read_size):
@@ -17,15 +18,15 @@ class reader_thread(threading.Thread):
 		read_from_file(self.fo,self.rs,self.wq)
 
 class writer_thread(threading.Thread):
-	def __init__(self,threadID,name,write_queue,file_object,read_size):
+	def __init__(self,threadID,name,crypt_queue,file_object,read_size):
 		threading.Thread.__init__(self)
 		self.ID=threadID
 		self.name=name
-		self.wq=write_queue
+		self.cq=crypt_queue
 		self.fo=file_object
 		self.rs=read_size
 	def run(self):
-		
+		write_to_file(self.fo,read_size,self.cq)
 class crypter_thread(threading.Thread):
 	def __init__(self,threadID,name,alphabet,key,write_queue,crypt_queue):
 		threading.Thread.__init__(self)
@@ -40,7 +41,7 @@ class crypter_thread(threading.Thread):
 		
 
 def crypt_string(alphabet,key,write_queue,crypt_queue):
-	while not True:
+	while True:
 		queue_lock_read.acquire()
 		if not write_queue.empty()
 			text=write_queue.get()
@@ -52,9 +53,7 @@ def crypt_string(alphabet,key,write_queue,crypt_queue):
 				else:
 					data+=c
 			crypt=(data,text[1])
-			queue_lock_crypt.acquire()
 			crypt_queue.put(crypt)
-			queue_lock_crypt.release()
 		else:
 			queue_lock_read.release()
 			if write_finished:
@@ -62,7 +61,7 @@ def crypt_string(alphabet,key,write_queue,crypt_queue):
 						
 def read_from_file(file_object,read_size,write_queue):
 	index=1
-	while(True)
+	while True
 		text=fo.read(read_size)
 		if(text==''):
 			break
@@ -72,12 +71,19 @@ def read_from_file(file_object,read_size,write_queue):
 	global write_finished
 	write_finished=True
 	
-def write_to_file(file_object,write_size,crypt_queue):
+def write_to_file(file_object,crypt_queue):
 	index=1
-	while(not crypt_queue.empty())
-		data=crypt_queue.get()	
-		file_object.write(data[0])
-		index+=1
+	while True:
+		queue_lock_crypt.acquire()
+		if not crypt_queue.empty():
+			data=crypt_queue.get()	
+			queue_lock_crypt.release()
+			file_object.write(data[0])
+			index+=1
+		else:
+			queue_lock_crypt.release()
+			time.sleep(1)
+
 if len(sys.argv) != 4:
 	print "Usage: python odev03_thread.py <shifting> <threads> <block_length>"
 	sys.exit("Invalid arguments")
