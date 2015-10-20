a789f660f84af15f0dccff8c3c6c90ab7eb07b62
volaka <hasancanvolaka@gmail.com>
2015-10-20 12:51:15 +0300

    MAJOR CHANGES! ---------------------------------------- -fonksiyonlar threadlerin icine tasindi.
    -sifrelenecek karakter dizileri kuyruktan alinip baska bir kuyruga konulmaktaydi.
    hala kuyruktan alinmaktalar fakat konulduklari yer kuyruk degil liste olarak degistirildi.
    
    -bunun sebebi fifo kuyruklarinda ilk girenin ilk cikmasidir.
    dosyadan okunan her blok okunma sirasina gore indexleniyor.
    sifrelerken de bu index degismiyor ve dosyaya yazma sirasinda bu index'e gore dosyaya yaziliyor.
    4 thread ayni anda calistiginda index sirasina gore sifreleme yapilacaginin garantisi yok.
    bu yuzden kuyruk kullanildiginda dogru indexli (tuple)'i bulmak icin cok sayida get ve put islemi yapmak gerekebilir.
    bu da hem algoritma acisindan hem de sure acisindan verimsiz oldu.
    bu sebeple ikinci kuyrugu listeye cevirerek listeden get put yapmadan icinden istenilen index'li (tuple) bulunuyor;
    ve bulundutan sonra pop fonksiyonu ile listeden cıkartiliyor.

--- odev03/odev03_thread.py
+++ odev03/odev03_thread.py
@@ -7,85 +7,79 @@ import time
 
 class reader_thread(threading.Thread):
 	def __init__(self,threadID,name,file_object,write_queue,read_size):
-		threading.Thread.__init(self)
+		threading.Thread.__init__(self)
 		self.threadID=threadID
 		self.name=name
 		self.fo=file_object
 		self.wq=write_queue
 		self.rs=read_size
 	def run(self):
-		read_from_file(self.fo,self.rs,self.wq)
+		index=1
+		while True:
+			text=self.fo.read(self.rs)
+			if(text==''):
+				break
+			text_tup=(text.lower(),index)
+			self.wq.put(text_tup)
+			index+=1
+		global write_finished
+		write_finished=True
 
 class writer_thread(threading.Thread):
-	def __init__(self,threadID,name,crypt_queue,file_object):
+	def __init__(self,threadID,name,crypt_list,file_object):
 		threading.Thread.__init__(self)
 		self.ID=threadID
 		self.name=name
-		self.cq=crypt_queue
+		self.cl=crypt_list
 		self.fo=file_object
 	def run(self):
-		write_to_file(self.fo,self.cq)
+		index=1
+		while True:
+			queue_lock_crypt.acquire()
+			if len(self.cl) !=0:
+				for l in self.cl:
+					if l[1]==index:
+						data=self.cl.pop(self.cl.index(l))
+						queue_lock_crypt.release()
+						self.fo.write(data[0])
+						index+=1
+						break
+			else:
+				queue_lock_crypt.release()
+				if write_finished==True:
+					break
+
 class crypter_thread(threading.Thread):
-	def __init__(self,threadID,name,alphabet,key,write_queue,crypt_queue):
+	def __init__(self,threadID,name,alphabet,key,write_queue,crypt_list):
 		threading.Thread.__init__(self)
 		self.threadID=threadID
 		self.name=name
 		self.alphabet=alphabet
 		self.key=key
-		self.cq=crypt_queue
+		self.cl=crypt_list
 		self.wq=write_queue
 	def run(self):
-		crypt_string(self.alphabet,self.key,self.wq,self.cq)
-		
-
-def crypt_string(alphabet,key,write_queue,crypt_queue):
-	while True:
-		queue_lock_read.acquire()
-		if not write_queue.empty():
-			text=write_queue.get()
-			queue_lock_read.release()
-			data=''
-			for c in text[0]:
-				if c in alphabet:
-					data+=key[alphabet.find(c)]
-				else:
-					data+=c
-			crypt=(data,text[1])
-			crypt_queue.put(crypt)
-		else:
-			queue_lock_read.release()
-			if write_finished:
-				break
-						
-def read_from_file(file_object,read_size,write_queue):
-	index=1
-	while True:
-		text=fo.read(read_size)
-		if(text==''):
-			break
-		text=(text.lower,index)
-		write_queue.put(text)
-		index+=1
-	global write_finished
-	write_finished=True
-	
-def write_to_file(file_object,crypt_queue):
-	index=1
-	found=False
-	while True:
-		queue_lock_crypt.acquire()
-		if not crypt_queue.empty():
-			while not found:
-				data=crypt_queue.get()
-				if data[1]==index:
-					file_object.write(data[0])
-					queue_lock_crypt.release()
-					found=True
-				else:
-					crypt_queue.put(data)
-		else:
-			queue_lock_crypt.release()
-			time.sleep(1)
+		while True:
+			queue_lock_read.acquire()
+			if not self.wq.empty():
+				text=self.wq.get()
+				queue_lock_read.release()
+				data=''
+				for c in text[0]:
+					if c in self.alphabet:
+						data+=self.key[self.alphabet.find(c)]
+					else:
+						data+=c
+				crypt=(data,text[1])
+				queue_lock_crypt.acquire()
+				self.cl.append(crypt)
+				queue_lock_crypt.release()
+			else:
+				queue_lock_read.release()
+				if write_finished:
+					break
+				
+				
 
 if len(sys.argv) != 4:
 	print "Usage: python odev03_thread.py <shifting> <threads> <block_length>"
@@ -103,10 +97,10 @@ else:
 	queue_lock_read=threading.Lock()
 	queue_lock_crypt=threading.Lock()
 	write_queue=Queue.Queue(10)
-	crypt_queue=Queue.Queue(10)
+	crypt_list=[]
 	
 	write_finished=False
-	
+	exit_writing=False
 	f_read=open("metin.txt","rb")
 	write_file="crypted_"+str(s)+"_"+str(n)+"_"+str(l)+".txt"
 	f_write=open(write_file,"wb")
@@ -114,13 +108,16 @@ else:
 	threads=[]
 	thread=reader_thread(1,"Reader Thread",f_read,write_queue,l)
 	thread.start()
+	print thread.name + " basladi"
 	threads.append(thread)
 	for i in range(1,n+1):
-		thread=crypter_thread(i+1,"Crypter Thread"+str(i),alphabet,key,write_queue,crypt_queue)
+		thread=crypter_thread(i+1,"Crypter Thread"+str(i),alphabet,key,write_queue,crypt_list)
 		thread.start()
+		print thread.name + " basladi"
 		threads.append(thread)
-	thread=writer_thread(i+2,"Writer Thread",crypt_queue,f_write)
+	thread=writer_thread(i+2,"Writer Thread",crypt_list,f_write)
 	thread.start()
+	print thread.name + " basladi"
 	threads.append(thread)
 	
 	for t in threads:
