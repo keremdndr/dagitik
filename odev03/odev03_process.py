3f0e6f72f979b04c8c523ff67bd4edd5afaaf55f
volaka <hasancanvolaka@gmail.com>
2015-10-22 14:45:13 +0300

    crypter ve reader process inin icindeki deadlocklar duzeltildi. writer process calismamakta.

--- odev03/odev03_process.py
+++ odev03/odev03_process.py
@@ -2,87 +2,124 @@
 
 import sys
 import time
-from multiprocessing import Process,Queue,Lock,current_process()
+from multiprocessing import Process,Queue,Lock,current_process
 
 def reader(read_size,read_queue):
 	try:
 		p=current_process()
 		file_r=open("metin.txt","rb")
-		data='x'
+		data=''
 		index=1
-		while data!='':
-			print p.name
-			data=file_r.read(read_size)
-			data_i=(data,index)
-			read_queue.put(data_i)
-			index+=1
+		while True:
+			#read_lock.acquire()
+			#if read_queue.full():
+			#	read_lock.release()
+			#	time.sleep(0.2)
+			#else:
+				data=file_r.read(read_size)
+				if data=='':
+					break
+				data_i=(data,index)
+				#print "Writing to read queue",index
+				read_queue.put(data_i)
+				#read_lock.release()
+				index+=1
 		file_r.close()
-	except Ex:
+	except Exception:
 		file_r.close()
-		print "in "+p.name+" error "+ Ex.strerror 
-	
+		print "in "+p.name+" error "
+	print "Reading Finished-----------------------------------------------------------"
+	read_queue.put(('',0))
+ 
 def crypter(alphabet,key,read_queue,crypt_queue):
+	p=current_process()
+	print p.name	
 	data=''
 	crypt=''
-	try:
+	while True:
 		read_lock.acquire()
-		while not read_queue.empty():
+		if read_queue.empty():
+			#print "Read queue empty!!!!!"
+			read_lock.release()
+			time.sleep(0.2)
+		else:
 			text=read_queue.get()
+			#print "Reading from READ queue",current_process().name,text[1]
+			#print text, current_process().name
+			#print text[1]
+			if text[1]==0:
+				read_queue.put(text)
+				read_lock.release()
+				break
 			read_lock.release()
 			for c in text[0]:
 				if c in alphabet:
 					data+=key[alphabet.find(c)]
 				else:
 					data+=c
-				
-				crypt=(data,text[1])
-				crypt_lock.acquire()
-				crypt_queue.put(crypt)
-				crypt_lock.release()
-		crypt_queue.put(('',-1))
+			
+			crypt=(data,text[1])
+			#print "Writing to CRYPT queue",current_process().name,crypt[1]
+	crypt_queue.put(('',-1))
+	print p.name,"Crypting finished"
+	
 
-
-def writer(crypt_queue,filename):
+def writer(crypt_queue,filename,thread_count):
 	index=1
 	f_write=open(filename,"wb")
 	crypt_list=[]
 	found=False
+	finish=False
 	while not finish:
 		if len(crypt_list)!=0:
 			for c in crypt_list:
 				if c[1]==index:
 					found=True
 					f_write.write(c[0])
-					crypt_list.pop(crypt_list.index(c)j)
+					print "Writing to file"
+					crypt_list.pop(crypt_list.index(c))					
 					index+=1
 					break
 			if found:
 				found=False
 			else:
+				crypt_lock.acquire()
+				if crypt_queue.empty():
+					crypt_lock.release()
+					time.sleep(0.1)
+					continue
+				print "Reading from CRYPT queue"
 				crypt=crypt_queue.get()
+				crypt_lock.release()
 				if crypt[1]==index:
 					f_write.write(crypt[0])
+					print "Writing to file"
 					index+=1
 				else:
 					crypt_list.append(crypt)
 		else:
+			crypt_lock.acquire()
+			if crypt_queue.empty():
+				crypt_lock.release()
+				time.sleep(0.2)
+				continue
 			crypt=crypt_queue.get()
+			crypt_lock.release()
 			if crypt[1]==index:
-				f_write.write(crypt)
+				f_write.write(crypt[0])
+				print "Writing to file"
 				index+=1
 			else:
 				crypt_list.append(crypt)
-		
-		
-		
-		if len(crypt_list)==4:
+		if len(crypt_list)==thread_count:
 			for c in crypt_list:
 				if c[1]!=-1:
 					finish=False
 					break
 				finish=True
-					
-def main():
+	print "Writing Finished"
+
+if __name__=='__main__':
 	if len(sys.argv) != 4:
 		print "Usage: python odev03_process.py <shifting> <threads> <block_length>"
 		sys.exit("Invalid arguments")
@@ -96,8 +133,8 @@ def main():
 		key=tail+head
 		key=key.upper()
 		
-		read_queue=Queue()
-		crypt_queue=Queue()
+		read_queue=Queue(400)
+		crypt_queue=Queue(400)
 		read_lock=Lock()
 		crypt_lock=Lock()
 		
@@ -110,7 +147,13 @@ def main():
 			p=Process(name='Crypter Process '+str(i+1),target=crypter,args=(alphabet,key,read_queue,crypt_queue,))
 			p.start()
 			processes.append(p)
-		
-		writer_p=Process(name='Writer Process',target=writer,args=(crypt_queue,write_file_name,)
-if __name__ == '__main__':
-	main()	
+		writer_p=Process(name='Writer Process',target=writer,args=(crypt_queue,write_file_name,n,))
+		writer_p.start()		
+		reader_p.join()
+		for p in processes:
+			p.join
+		writer_p.join()
+		print "Exiting"		
+
+
+
