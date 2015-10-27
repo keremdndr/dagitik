dad1c1bf7289cde46bb5caba400dc08d20f67d80
volaka <hasancanvolaka@gmail.com>
2015-10-27 16:28:52 +0300

    thread yazildi. ana thread yazildi. ayri ayri data lam ve data gonderme test edildi.

--- odev04/server.py
+++ odev04/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import threading
 import socket
-from time import asctime
+from time import asctime,sleep
 from random import randint
 class myThread (threading.Thread):
 	def __init__(self, threadID, clientSocket, clientAddr):
@@ -11,20 +11,35 @@ class myThread (threading.Thread):
 		self.clientAddr = clientAddr
 	def run(self):
 		print "Starting Thread-" + str(self.threadID)
-		while True:
-			rand = randint(1,100)
-			wait(rand) 
-			self.clientSocket.sendall("Merhaba, saat suan" + time.asctime())
+		try:
+			while True:
+				asd=self.clientSocket.recv(1024)
+				print asd
+				#self.clientSocket.sendall("Merhaba, saat suan" + asctime())
+		except Exception,Argument:
+			print "hata thread:",self.threadID,":",Argument 
 		print "Ending Thread-" + str(self.threadID)
-s = socket.socket()
-host = socket.gethostname()
+threadCounter=0
 port = 12345
-s.bind((host, port))
-s.listen(5)
-while True:
-	print "Waiting for connection"
-	c, addr = s.accept()
-	print 'Got a connection from ', addr
-	threadCounter += 1
-	thread = myThread(threadCounter, c, addr)
-	thread.start()
+threads=[]
+try:
+	s = socket.socket()
+	host = socket.gethostname()
+	s.bind((host, port))
+	s.listen(5)
+	while True:
+		print "Waiting for connection"
+		c, addr = s.accept()
+		print 'Got a connection from ', addr
+		threadCounter += 1
+		thread = myThread(threadCounter, c, addr)
+		thread.start()
+		threads.append(thread)
+except Exception,Argument:
+	print Argument
+finally:
+	s.close()
+	for t in threads:
+		t.join()
+	print "exiting"
+	
