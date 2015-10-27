5b9d34db9f8db9ef9bd175dfb87eaaba81873317
volaka <hasancanvolaka@gmail.com>
2015-10-27 16:29:25 +0300

    iki thread dolduruldu. client script duzgun calismakta.

--- odev04/client.py
+++ odev04/client.py
@@ -1,25 +1,52 @@
 #!/usr/bin/env python
 import socket
 import threading
-Import random
+
 class readThread (threading.Thread):
-	...
-	...
-	...
+	def __init__(self,servSock):
+		threading.Thread.__init__(self)
+		self.s=servSock
+	def run(self):
+		try:
+			while True:
+				rec = self.s.recv(1024)
+				print "Server:",rec
+		except Exception,Argument:
+			print Argument
 class writeThread (threading.Thread):
-	...
-	...
-	...
-s = socket.socket()
-host = "127.0.0.1"
-port = 12345
-s.connect((host, port))
-...
-...
-...
-rThread = readThread(...)
-rThread.start()
-wThread = writeThread(...)
-wThread.start()
-...
-...
+	def __init__(self,servSock,inp):
+		threading.Thread.__init__(self)
+		self.s=servSock
+		self.inp=inp
+	def run(self):
+		try:
+			self.s.sendall(inp)
+		except Exception,Argument:
+			print Argument
+try:
+	s = socket.socket()
+	host = socket.gethostname()
+	port = 12345
+	s.connect((host, port))
+
+	rThread = readThread(s)
+	rThread.start()
+	print "To Exit, enter 'exit'"
+	while True:
+		inp=raw_input(">>")
+		print inp
+		if inp.lower()=="exit":
+			s.close()
+			break
+		else:
+			wThread = writeThread(s,inp)
+			wThread.start()
+except KeyboardInterrupt:
+	print "Keyboard Interrupt"
+except Exception:
+	print "Hata Main"
+finally:
+	s.close()
+	rThread.join()
+	print "Read Thread Finished"
+print "Exiting"
