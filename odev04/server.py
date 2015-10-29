ec36219aefe835d5518d149dff33c3fd41646031
volaka <hasancanvolaka@gmail.com>
2015-10-29 11:28:59 +0300

    exit geldiginde thread sonlandirilmaya calisildi. fakat thread sonlandirilsa da main thread accept methodunda blocklanmis durumda.

--- odev04/server.py
+++ odev04/server.py
@@ -3,7 +3,20 @@ import threading
 import socket
 from time import asctime,sleep
 from random import randint
-class myThread (threading.Thread):
+class serverSend (threading.Thread):
+	def __init__(self,threadID,clientSocket,clientAddr):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.clientSocket = clientSocket
+		self.clientAddr= clientAddr
+	def run(self):
+		try:
+			while not Stop:
+				sleep(randint(1,60))
+				self.clientSocket.sendall("Merhaba, saat suan " + asctime())
+		except Exception, e:
+			print "Hata thread",threadID,":",e
+class serverReceive (threading.Thread):
 	def __init__(self, threadID, clientSocket, clientAddr):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
@@ -12,16 +25,20 @@ class myThread (threading.Thread):
 	def run(self):
 		print "Starting Thread-" + str(self.threadID)
 		try:
-			while True:
-				asd=self.clientSocket.recv(1024)
-				print asd
-				#self.clientSocket.sendall("Merhaba, saat suan" + asctime())
-		except Exception,Argument:
-			print "hata thread:",self.threadID,":",Argument 
+			global Stop
+			while not Stop:
+				rec=self.clientSocket.recv(1024)
+				if rec != "":
+					print rec
+				elif rec == "exit":
+					Stop = True
+		except Exception,e:
+			print "Hata thread",self.threadID,":",e 
 		print "Ending Thread-" + str(self.threadID)
 threadCounter=0
 port = 12345
 threads=[]
+Stop=False
 try:
 	s = socket.socket()
 	host = socket.gethostname()
@@ -32,12 +49,17 @@ try:
 		c, addr = s.accept()
 		print 'Got a connection from ', addr
 		threadCounter += 1
-		thread = myThread(threadCounter, c, addr)
+		thread = serverReceive(threadCounter, c, addr)
+		thread.start()
+		threads.append(thread)
+		threadCounter +=1
+		thread = serverSend(threadCounter,c,addr)
 		thread.start()
 		threads.append(thread)
 except Exception,Argument:
 	print Argument
 finally:
+	Stop=True
 	s.close()
 	for t in threads:
 		t.join()
