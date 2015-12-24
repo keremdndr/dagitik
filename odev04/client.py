304c46ff07a053585d476b11b7278705997fe51d
volaka <hasancanvolaka@gmail.com>
2015-12-24 14:20:48 +0200

    file reformatted

--- odev04/client.py
+++ odev04/client.py
@@ -2,51 +2,58 @@
 import socket
 import threading
 
-class readThread (threading.Thread):
-	def __init__(self,servSock):
-		threading.Thread.__init__(self)
-		self.s=servSock
-	def run(self):
-		try:
-			while not Stop:
-				rec = self.s.recv(1024)
-				print "Server:",rec
-		except Exception,Argument:
-			print Argument
-class writeThread (threading.Thread):
-	def __init__(self,servSock,inp):
-		threading.Thread.__init__(self)
-		self.s=servSock
-		self.inp=inp
-	def run(self):
-		try:
-			self.s.sendall(inp)
-		except Exception,Argument:
-			print Argument
+
+class readThread(threading.Thread):
+    def __init__(self, servSock):
+        threading.Thread.__init__(self)
+        self.s = servSock
+
+    def run(self):
+        try:
+            while not Stop:
+                rec = self.s.recv(1024)
+                print "Server:", rec
+        except Exception, Argument:
+            print Argument
+
+
+class writeThread(threading.Thread):
+    def __init__(self, servSock, inp):
+        threading.Thread.__init__(self)
+        self.s = servSock
+        self.inp = inp
+
+    def run(self):
+        try:
+            self.s.sendall(inp)
+        except Exception, Argument:
+            print Argument
+
+
 Stop = False
 try:
-	s = socket.socket()
-	host = socket.gethostname()
-	port = 12345
-	s.connect((host, port))
+    s = socket.socket()
+    host = socket.gethostname()
+    port = 12345
+    s.connect((host, port))
 
-	rThread = readThread(s)
-	rThread.start()
-	print "To Exit, enter 'eof'"
-	while True:
-		inp=raw_input(">>")
-		wThread = writeThread(s,inp)
-		wThread.start()
-		if inp.lower()=="eof":
-			Stop = True
-			break
+    rThread = readThread(s)
+    rThread.start()
+    print "To Exit, enter 'eof'"
+    while True:
+        inp = raw_input(">>")
+        wThread = writeThread(s, inp)
+        wThread.start()
+        if inp.lower() == "eof":
+            Stop = True
+            break
 
 except KeyboardInterrupt:
-	print "Keyboard Interrupt"
+    print "Keyboard Interrupt"
 except Exception:
-	print "Hata Main"
+    print "Hata Main"
 finally:
-	s.close()
-	rThread.join()
-	print "Read Thread Finished"
+    s.close()
+    rThread.join()
+    print "Read Thread Finished"
 print "Exiting"
--- odev04/server.py
+++ odev04/server.py
@@ -1,73 +1,80 @@
 #!/usr/bin/env python
 import threading
 import socket
-from time import asctime,sleep
+from time import asctime, sleep
 from random import randint
 
 Stop = False
-class serverSend (threading.Thread):
-	def __init__(self,threadID,clientSocket,clientAddr):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.clientSocket = clientSocket
-		self.clientAddr= clientAddr
-	def run(self):
-		try:
-			while not Stop:
-				sleep(randint(1,60))
-				if Stop:
-					break
-				self.clientSocket.sendall("Merhaba, saat suan " + asctime())
-		except Exception, e:
-			print "Hata thread sender",threadID,":",e
-		print "Sender Thread",threadID,"sonlandirildi"
-class serverReceive (threading.Thread):
-	def __init__(self, threadID, clientSocket, clientAddr):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.clientSocket = clientSocket
-		self.clientAddr = clientAddr
-	def run(self):
-		print "Starting Thread-" + str(self.threadID)
-		try:
-			while True:
-				rec=self.clientSocket.recv(1024)
-				if rec == "eof":
-					global Stop
-					Stop = True
-					break
-				elif rec != "":
-					print rec			
-					ans="Peki "+str(self.clientAddr[0])+":"+str(self.clientAddr[1])
-					self.clientSocket.sendall(ans)
-					
-		except Exception,e:
-			print "Hata thread receiver",str(self.threadID),":",e 
-		print "Ending Thread-",str(self.threadID)
-threadCounter=0
-port = 12345
-threads=[]
-try:
-	s=socket.socket()
-	host=socket.gethostname()
-	s.bind((host,port))
-	s.listen(5)
-except Exception,e:
-	print e
-while True:
-	try:
-		print "Waiting for connection"
-		c, addr = s.accept()
-		print 'Got a connection from ', addr
-		threadCounter += 1
-		thread = serverReceive(threadCounter, c, addr)
-		thread.start()
-		threads.append(thread)
-		thread = serverSend(threadCounter,c,addr)
-		thread.start()
-		threads.append(thread)
-	except Exception,Argument:
-		print Argument
-		s.close()
-		break
 
+
+class serverSend(threading.Thread):
+    def __init__(self, threadID, clientSocket, clientAddr):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.clientSocket = clientSocket
+        self.clientAddr = clientAddr
+
+    def run(self):
+        try:
+            while not Stop:
+                sleep(randint(1, 60))
+                if Stop:
+                    break
+                self.clientSocket.sendall("Merhaba, saat suan " + asctime())
+        except Exception, e:
+            print "Hata thread sender", threadID, ":", e
+        print "Sender Thread", threadID, "sonlandirildi"
+
+
+class serverReceive(threading.Thread):
+    def __init__(self, threadID, clientSocket, clientAddr):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.clientSocket = clientSocket
+        self.clientAddr = clientAddr
+
+    def run(self):
+        print "Starting Thread-" + str(self.threadID)
+        try:
+            while True:
+                rec = self.clientSocket.recv(1024)
+                if rec == "eof":
+                    global Stop
+                    Stop = True
+                    break
+                elif rec != "":
+                    print rec
+                    ans = "Peki " + str(self.clientAddr[0]) + ":" + str(self.clientAddr[1])
+                    self.clientSocket.sendall(ans)
+
+        except Exception, e:
+            print "Hata thread receiver", str(self.threadID), ":", e
+        print "Ending Thread-", str(self.threadID)
+
+
+threadCounter = 0
+port = 12345
+threads = []
+try:
+    s = socket.socket()
+    host = socket.gethostname()
+    s.bind((host, port))
+    s.listen(5)
+except Exception, e:
+    print e
+while True:
+    try:
+        print "Waiting for connection"
+        c, addr = s.accept()
+        print 'Got a connection from ', addr
+        threadCounter += 1
+        thread = serverReceive(threadCounter, c, addr)
+        thread.start()
+        threads.append(thread)
+        thread = serverSend(threadCounter, c, addr)
+        thread.start()
+        threads.append(thread)
+    except Exception, Argument:
+        print Argument
+        s.close()
+        break
