cb0e489db905b8d09072e96ce35ec94bca0b3c05
volaka <hasancanvolaka@gmail.com>
2015-12-24 14:48:09 +0200

    server thread is created as skeleton. tcp connection has been initialised.

--- proje/peer.py
+++ proje/peer.py
@@ -1,10 +1,15 @@
 import threading
+from _socket import gethostname
+from random import randint
 from socket import socket
+from twisted.python.util import println
 
 
 class ServerThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, server_socket):
         super(ServerThread, self).__init__()
+        self.sock = server_socket
+        print "Server is waiting for new connection..."
 
     def parser(self, request):
         request = request.strip()
@@ -41,7 +46,12 @@ class ServerThread(threading.Thread):
             pass
 
     def run(self):
-        pass
+        while True:
+            try:
+                data = self.sock.recv(1024)
+                self.parser(data)
+            except Exception, ex:
+                print ex.message
 
 
 class ClientThread(threading.Thread):
@@ -53,7 +63,23 @@ class ClientThread(threading.Thread):
 
 
 def main():
-    pass
+    host = gethostname()
+    server_socket = socket()
+    port = randint(50000, 65000)
+    try:
+        server_socket.bind((host, port))
+        server_socket.listen(5)
+
+        server_thread = ServerThread(server_socket)
+        server_thread.run()
+
+        client_thread = ClientThread()
+        client_thread.run()
+
+
+    except Exception, ex:
+        print ex.message
+
 
 
 if __name__ == '__main__':
